936e15374c7c11b6d15e699b43a2bcc6097885ae
Matthieu <matthieu.larose@student.ap.be>
2022-11-17 15:02:34 +0100

    Setup project

---
+++ src/main/java/moneytracker/Main.java
@@ -0,0 +1,21 @@
+package main.java.moneytracker;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Main main = new Main();
+        main.run();
+    }
+
+    public Main()
+    {
+
+    }
+
+    public void run()
+    {
+        
+    }
+}
